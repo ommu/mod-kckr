11817ed444f4db043cfa8ac539b90f6f4b343c96
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-09-16 23:08:24 +0700

    setting: crud source

---
+++ modules/kckr/views/o/setting/_form.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Kckr Settings (kckr-setting)
+ * @var $this SettingController
+ * @var $model KckrSetting
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 16 September 2016, 23:07 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+?>
+
+<?php $form=$this->beginWidget('application.components.system.OActiveForm', array(
+	'id'=>'kckr-setting-form',
+	'enableAjaxValidation'=>true,
+	//'htmlOptions' => array('enctype' => 'multipart/form-data')
+)); ?>
+
+<?php //begin.Messages ?>
+<div id="ajax-message">
+	<?php echo $form->errorSummary($model); ?>
+</div>
+<?php //begin.Messages ?>
+
+<fieldset>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'license'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'license',array('size'=>32,'maxlength'=>32)); ?>
+			<?php echo $form->error($model,'license'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix publish">
+		<?php echo $form->labelEx($model,'permission'); ?>
+		<div class="desc">
+			<?php echo $form->checkBox($model,'permission'); ?>
+			<?php echo $form->labelEx($model,'permission'); ?>
+			<?php echo $form->error($model,'permission'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'meta_keyword'); ?>
+		<div class="desc">
+			<?php echo $form->textArea($model,'meta_keyword',array('rows'=>6, 'cols'=>50)); ?>
+			<?php echo $form->error($model,'meta_keyword'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'meta_description'); ?>
+		<div class="desc">
+			<?php echo $form->textArea($model,'meta_description',array('rows'=>6, 'cols'=>50)); ?>
+			<?php echo $form->error($model,'meta_description'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'modified_date'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'modified_date'); ?>
+			<?php echo $form->error($model,'modified_date'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'modified_id'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'modified_id',array('size'=>11,'maxlength'=>11)); ?>
+			<?php echo $form->error($model,'modified_id'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="submit clearfix">
+		<label>&nbsp;</label>
+		<div class="desc">
+			<?php echo CHtml::submitButton($model->isNewRecord ? Yii::t('phrase', 'Create') : Yii::t('phrase', 'Save'), array('onclick' => 'setEnableSave()')); ?>
+		</div>
+	</div>
+
+</fieldset>
+<?php /*
+<div class="dialog-content">
+</div>
+<div class="dialog-submit">
+	<?php echo CHtml::submitButton($model->isNewRecord ? Yii::t('phrase', 'Create') : Yii::t('phrase', 'Save') ,array('onclick' => 'setEnableSave()')); ?>
+	<?php echo CHtml::button(Yii::t('phrase', 'Cancel'), array('id'=>'closed')); ?>
+</div>
+*/?>
+<?php $this->endWidget(); ?>
+
+
---
+++ modules/kckr/views/o/setting/admin_add.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Kckr Settings (kckr-setting)
+ * @var $this SettingController
+ * @var $model KckrSetting
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 16 September 2016, 23:07 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Kckr Settings'=>array('manage'),
+		'Create',
+	);
+?>
+
+<div class="form">
+	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+</div>
